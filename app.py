c1b813f955a076e984454c42c636ea4c3eef66c7
Quynh <hp.quynh@yahoo.com>
2021-04-19 15:38:40 -0700

    add custom portfolio graph, fix layout

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import time
 import dash_html_components as html
 
 from datetime import date
+from datetime import datetime
 import dash_core_components as dcc
 import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output, State
@@ -15,7 +16,7 @@ import plotly.graph_objects as go
 
 app = dash.Dash(__name__)
 
-start = datetime.datetime(2000, 1, 1)
+start = datetime(2000, 1, 1)
 today = date.today()
 
 symb = ['AAPL', 'MSFT', 'TSLA', 'AMZN', 'FB']
@@ -44,8 +45,11 @@ app.index_string = '''
             {%config%}
             {%scripts%}
             {%renderer%}
+            <p>By: 
+            <a href="https://hpquynhuw.github.io/">Quynh Huynh</a> <br>
+            Last Updated: April 19, 2021 </p>
         </footer>
-        <div>Last Updated: March 24, 2021</div>
+        
     </body>
 </html>
 '''
@@ -71,7 +75,9 @@ def give_weekly_returns(series):
 for x in symb:
     globals()[x + '_weekly_returns'] = give_weekly_returns(globals()[x])
     globals()[x + '_monthly_returns'] = give_monthly_returns(globals()[x])
-    globals()[x + '_daily_returns'] = globals()[x].pct_change().dropna(how="all")
+    globals()[x + '_daily_returns'] = globals()[x].pct_change()
+
+all_daily_returns = data.pct_change()
 
 dropdowns = dcc.Dropdown(
     id='selectors',
@@ -122,7 +128,6 @@ def render_years(comp):
     Input('selector', 'value'))
 def update_graph1(comp):
     returns = globals()[comp + '_monthly_returns']  # insert index slice for time range here
-    print(returns.head())
     months_index = returns.index
     pos = 100 * returns[returns >= 0]
     neg = 100 * returns[returns < 0]
@@ -133,7 +138,7 @@ def update_graph1(comp):
     fig.update_layout(showlegend=False, template='plotly_white', title=comp + ' Monthly Returns History')
     fig.update_yaxes(range=[-40, 40], title_text='Returns %')
     fig.update_xaxes(rangeslider_visible=True)
-    fig.update_traces(hovertemplate='Returns: %{y:.2f}%')
+    fig.update_traces(hovertemplate='Returns: %{y:.2f}%  <br>Date: %{x}')
     fig.update_layout(hovermode="x unified")
     fig.update_layout(margin={'l': 10, 'b': 40, 't': 60, 'r': 10})
     return fig
@@ -155,13 +160,14 @@ def update_graph2(yrs, comp):
         else:
             fig.add_trace(go.Scatter(x=months, y=100 * y['Adj Close'],
                                      mode='lines+markers', name=yr))
-    fig.update_layout(showlegend=True, template='plotly_white', title='Comparing ' + comp + ' annual monthly returns')
+    fig.update_layout(showlegend=True, template='plotly_white',
+                      title='Comparing ' + comp + ' monthly returns between years')
     fig.update_xaxes(title_text='Months')
     fig.update_yaxes(title_text='Returns %')
     fig.update_traces(hovertemplate='Returns: %{y:.2f}%')
     fig.update_layout(hovermode="x unified")
     fig.update_layout(legend_x=0, legend_y=1)
-    fig.update_layout(height=220, margin={'l': 30, 'b': 30, 'r': 0, 't': 50})
+    fig.update_layout(height=180, margin={'l': 30, 'b': 30, 'r': 0, 't': 50})
     return fig
 
 
@@ -173,23 +179,28 @@ def update_graph2(yrs, comp):
 def update_graph3(comp1, comp2, year):
     pdf = pd.DataFrame()
     pdf.columns.name = 'company'
-    pdf[comp1] = globals()[comp1 + '_weekly_returns'][year]
-    pdf[comp2] = globals()[comp2 + '_weekly_returns'][year]
+    pdf[comp1] = 100*globals()[comp1 + '_weekly_returns'][year]
+    pdf[comp2] = 100*globals()[comp2 + '_weekly_returns'][year]
     fig = px.area(pdf, facet_col="company", facet_col_wrap=1)
     title = "Weekly Returns of " + comp1 + " and " + comp2 + ' in ' + year
     fig.update_layout(xaxis_rangeslider_visible=True, title_text=title)
     fig.update_layout(template='plotly_white')
-    fig.update_traces(hovertemplate='Returns: %{y:.2f}%')
+    fig.update_traces(hovertemplate='Returns: %{y:.2f}% <br>Date: %{x}')
     fig.update_layout(hovermode="x unified")
     fig.update_layout(legend_x=0, legend_y=1)
-    fig.update_layout(height=220, margin={'l': 30, 'b': 30, 'r': 0, 't': 50})
+    fig.update_layout(height=260, margin={'l': 30, 'b': 30, 'r': 0, 't': 50})
     return fig
 
 
 app.layout = dbc.Container(
-    [html.H1('Hello Dash'),
+    [html.H1('Portfolio Visualizer'),
      html.Hr(),
-     html.H2('Dash: A web application framework for Python.'),
+     # html.Div(children = [
+     #      dcc.Store(id="figstore", data=go.Figure()),
+     #      dcc.Store(id="patchstore", data={}),
+     #      dcc.Store(id="patchstore2", data={})]),
+     html.H2('A Python interactive dashboard for simulating portfolio diversification.',
+             style={'padding-bottom': '0.5rem'}),
      dbc.Tabs(
          [
              dbc.Tab(label="Prices", tab_id="price"),
@@ -207,7 +218,7 @@ app.layout = dbc.Container(
                               dbc.Label("Portfolio start date")], width=6),
                               dbc.Col(children=[
                                   dbc.Label("Investment amount at start date",
-                                            style={'float': 'right', 'display': 'inline-block', 'margin-right':'0'})
+                                            style={'float': 'right', 'display': 'inline-block', 'margin-right': '0'})
                               ], width=6)]),
                           dbc.Row([dbc.Col(children=[
                               dcc.DatePickerSingle(
@@ -265,34 +276,133 @@ app.layout = dbc.Container(
                               ], style={'padding-top': '30px'}, width=4)
                           ])
                       ], width=6),
-                      dbc.Col(children=[],
-                              style={'width': '49%', 'float': 'right', 'display': 'inline-block'},
-                              width=6)
+                      dbc.Col(children=[
+                          dcc.Checklist(
+                              options=[
+                                  {'label': 'Show Efficient Frontier with max Sharpe ratio', 'value': 'max'},
+                                  {'label': 'Show Efficient Frontier with min volatility', 'value': 'min'},
+                              ],
+                              value=[],
+                              id='optimized-opts',
+                              labelStyle={'display': 'block'}
+                          ),
+                          html.P('Only invest in', style={'padding-top': '5px'}),
+                          dcc.Checklist(
+                              options=[
+                                  {'label': 'Apple Inc.', 'value': 'AAPL'},
+                                  {'label': 'Microsoft Corp.', 'value': 'MSFT'},
+                                  {'label': 'Tesla Inc.', 'value': 'TSLA'},
+                                  {'label': 'Amazon.com Inc.', 'value': 'AMZN'},
+                                  {'label': 'Facebook Inc.', 'value': 'FB'}
+                              ],
+                              id='no-div',
+                              value=[],
+                              labelStyle={'display': 'in-line block'}
+                          ),
+
+                      ],
+                          style={'width': '49%', 'float': 'right', 'display': 'inline-block'},
+                          width=6)
                   ],
                   style={
                       'borderBottom': 'thin lightgrey solid',
                       'backgroundColor': 'rgb(250, 250, 250)',
-                      'padding': '5px'
-                  }, align='center')]
+                      'padding': '20px'},
+                  align='center'),
+                  dcc.Graph(id='port-graph')],
+              style={'padding-top': '20px'}
               )]
 )
 
 
+#
+# deep_merge = """
+# function batchAssign(patches) {
+#     function recursiveAssign(input, patch){
+#         var outputR = Object(input);
+#         for (var key in patch) {
+#             if(outputR[key] && typeof patch[key] == "object") {
+#                 outputR[key] = recursiveAssign(outputR[key], patch[key])
+#             }
+#             else {
+#                 outputR[key] = patch[key];
+#             }
+#         }
+#         return outputR;
+#     }
+#
+#     return Array.prototype.reduce.call(arguments, recursiveAssign, {});
+# }
+# """
+#
+# app.clientside_callback(
+#     deep_merge,
+#     Output('price-graph', 'figure'),
+#     [Input('figstore', 'data'),
+#      Input('patchstore', 'data'),
+#      Input('patchstore2', 'data')])
+
+
 def calc_custom_port(val0, weights, rets):
-    calc = val0
+    calc = [val0]
     for i in range(1, len(rets)):
         ret0 = 1
         for j in symb:
             ret0 += weights[j] * rets[j][i]
         calc.append(calc[i - 1] * ret0)
+    return calc
 
 
-@app.callback(Output('output-state', 'children'),
-              Input('submit-button-state', 'n_clicks'),
-              State('input-1-state', 'value'),
-              State('input-2-state', 'value'))
-def update_output(n_clicks, input1, input2):
-    return
+@app.callback(Output('port-graph', 'figure'),
+              Input('submit-button', 'n_clicks'),
+              State('portfolio-date', 'date'),
+              State('fund1', 'value'),
+              State('w1', 'value'),
+              State('w2', 'value'),
+              State('w3', 'value'),
+              State('w4', 'value'),
+              State('w5', 'value'),
+              State('optimized-opts', 'value'),
+              State('no-div', 'value'))
+def port_graph(n_clicks, day0, fund, w1, w2, w3, w4, w5, pts, singles):
+    dt = datetime.strptime(day0, '%Y-%m-%d')
+    idx = data.index.get_loc(dt, method='nearest')
+    rets = all_daily_returns[idx:].fillna(0)
+    weights = {'AAPL': w1,
+               'MSFT': w2,
+               'TSLA': w3,
+               'AMZN': w4,
+               'FB': w5}
+
+    calc = calc_custom_port(fund, weights, rets)
+    fig = go.Figure()
+
+    fig.add_trace(go.Scatter(x=rets.index, y=calc,
+                             mode='lines', name='Custom Portfolio Value'))
+
+    if singles:
+        fig = add_line(fig, singles, fund, rets)
+
+    fig.update_xaxes(title_text='Date')
+    fig.update_yaxes(title_text='Value $')
+    fig.update_layout(legend_x=0, legend_y=1, template='plotly_white')
+    fig.update_traces(hovertemplate='Value: $%{y:.2f} <br>Date: %{x}')
+    fig.update_layout(hovermode="x unified", title='Portfolio Growth')
+
+    fig.update_xaxes(rangeslider_visible=True)
+    return fig
+
+
+def add_line(fig, comps, fund, rets):
+    for company in comps:
+        a = symb[:]
+        a.remove(company)
+        wts = {k: 0.0 for k in a}
+        wts[company] = 1.0
+        calc = calc_custom_port(fund, wts, rets)
+        fig.add_trace(go.Scatter(x=rets.index, y=calc,
+                                 mode='lines', name='Only invest in ' + company))
+    return fig
 
 
 @app.callback(
@@ -313,7 +423,7 @@ def render_tab_content(active_tab):
                     dropdowns
                 ],
                     style={'align': 'right', 'text-align': 'right', 'padding': '20px 100px 10px'}),
-                dcc.Graph(id='output-graph-range-slider')]
+                dcc.Graph(id='price-graph')]
             )],
             align='center')
     elif active_tab == "returns":
@@ -361,7 +471,7 @@ def render_tab_content(active_tab):
 
 
 @app.callback(
-    Output('output-graph-range-slider', 'figure'),
+    Output('price-graph', 'figure'),
     [Input('selectors', 'value')], )
 def update_price_history(selects):
     fig = go.Figure()
@@ -378,6 +488,25 @@ def update_price_history(selects):
 
     fig.update_xaxes(rangeslider_visible=True)
     return fig
+
+
+#
+# @app.callback(Output('patchstore2', 'data'), [Input('selectors', 'value')])
+# def update_price_history(selects):
+#     fig = go.Figure()
+#
+#     for x in selects:
+#         fig.add_trace(go.Scatter(x=data.index, y=data[x],
+#                                  mode='lines', name=x))
+#
+#     fig.update_xaxes(title_text='Date')
+#     fig.update_yaxes(title_text='Value $')
+#     fig.update_layout(legend_x=0, legend_y=1, template='plotly_white', title='Stock Prices History')
+#     fig.update_traces(hovertemplate='Price: $%{y:.2f} <br>Date: %{x}')
+#     fig.update_layout(hovermode="x unified")
+#
+#     fig.update_xaxes(rangeslider_visible=True)
+#     return fig
 
 
 if __name__ == '__main__':
